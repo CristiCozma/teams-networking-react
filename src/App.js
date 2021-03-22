aacd47cb400449c7d762a2a439be6b4991c0b23f
Cozma Cristian <cozmacristian@HUAWEI_P9_lite_mini.localdomain>
2021-03-22 20:10:19 +0200

    add Component as class + internal state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
+import { Component } from 'react';
 import './App.css';
 import { PersonsTable } from "./PersonsTable";
 
-let teams = [
-  {
-    "id": "abcdef1610310163146",
-    "firstName": "Nic",
-    "lastName": "Matei",
-    "gitHub": "nmatei"
-  },
-  {
-    "id": "qhejbm1610993148257",
-    "firstName": "Cristian",
-    "lastName": "Cozma",
-    "gitHub": "CristiCozma"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: []
+    }
   }
-];
 
-function App() {
-  return (
-    <div>
-      <h1>Teams Networking</h1>
-      <div>Search</div>
-      <PersonsTable teams={teams} border={1} />
-    </div>
-  );
+  componentDidMount() {
+    console.warn('mount');
+    setTimeout(() => {
+      console.warn('loaded');
+      this.setState({
+        teams: [
+          {
+            "id": "abcdef1610310163146",
+            "firstName": "Nic",
+            "lastName": "Matei",
+            "gitHub": "nmatei"
+          },
+          {
+            "id": "qhejbm1610993148257",
+            "firstName": "Cristian",
+            "lastName": "Cozma",
+            "gitHub": "CristiCozma"
+          }
+        ]
+      })
+    }, 2000);
+
+  }
+
+  render() {
+    console.debug(this.state.teams);
+    return (
+      <div>
+        <h1>Teams Networking</h1>
+        <div>Search</div>
+        <PersonsTable teams={this.state.teams} border={1} />
+      </div>
+    );
+  }
 }
 
 export default App;
