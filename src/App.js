99f67827b752cdaa290e269bae2c6e5a6f5f60e9
Cozma Cristian <cozmacristian@HUAWEI_P9_lite_mini.localdomain>
2021-03-29 19:57:43 +0300

    move teams in Redux.store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
 import { PersonsTable } from "./PersonsTable";
 
 class App extends Component {
@@ -7,49 +8,42 @@ class App extends Component {
     super(props);
     this.state = {
       teams: [],
-      date: new Date().toString()
-    }
+      date: new Date().toString(),
+    };
+    console.warn("props", props);
   }
 
   componentDidMount() {
     setInterval(() => {
       this.setState({
-        date: new Date().toString()
-      })
+        date: new Date().toString(),
+      });
     }, 60000);
 
     this.load();
   }
 
-  load() {
-    fetch("http://localhost:3000/teams-json")
-      .then(res => res.json())
-      .then(teams => {
-        this.setState({
-          teams
-        });
-      });
-  }
+  load() {}
 
   add(team) {
     console.warn("team", team);
-    document.getElementById('main-form').reset();
+    document.getElementById("main-form").reset();
 
     fetch("http://localhost:3000/teams-json/create", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(team)
+      body: JSON.stringify(team),
     })
-      .then(res => res.json())
-      .then(r => {
+      .then((res) => res.json())
+      .then((r) => {
         console.warn(r);
         if (r.success) {
           team.id = r.id;
           const teams = this.state.teams.concat(team);
           this.setState({
-            teams
+            teams,
           });
           //this.load();
         }
@@ -60,27 +54,28 @@ class App extends Component {
     fetch("http://localhost:3000/teams-json/delete", {
       method: "DELETE",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ id })
-    }).then(r => r.json()).then(status => {
-      this.load();
-    });
+      body: JSON.stringify({ id }),
+    })
+      .then((r) => r.json())
+      .then((status) => {
+        this.load();
+      });
   }
 
   render() {
-    console.debug(this.state.teams);
     return (
       <div>
         <h1>Teams Networking</h1>
         <div>Search</div>
         <PersonsTable
-          teams={this.state.teams}
+          teams={this.props.teams}
           border={1}
-          onSubmit={team => {
+          onSubmit={(team) => {
             this.add(team);
           }}
-          onDelete={id => {
+          onDelete={(id) => {
             this.remove(id);
           }}
         />
@@ -90,4 +85,13 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  console.info("map state to props", state);
+  return {
+    teams: state.teams,
+  };
+};
+
+const AppContainer = connect(mapStateToProps)(App);
+
+export default AppContainer;
